81c3f159be7b62a031a2cf4e97d223afadc245cc
Ibrahim <153442059+Ibrahim-Hurubi@users.noreply.github.com>
2026-03-17 06:25:51 +0300

    Add custom webpack configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
+
+    config.module.rules.push({
+      test: /charLS-DynamicMemory-browser\.js$/,
+      type: "javascript/auto",
+    });
+
+    return config;
   },
-  transpilePackages: ["lucide-react"],
 };
 
 export default nextConfig;
